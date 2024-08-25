2c9b38259b77837ac6ec2731c19ac2d42c02bab9
Wei Hung <wayne930242@gmail.com>
2024-08-26 01:41:35 +0800

    Fix origin

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const origin = requestUrl.origin;
+  const origin = "https://handouts.wayneh.tw";
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
 
   if (code) {
