33c49b84aed61af4c3b2de4786e040444d4aa334
Wei Hung <wayne930242@gmail.com>
2024-08-26 01:00:35 +0800

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,44 @@
 import { Noto_Sans_TC } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import { Metadata } from "next";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "ShareHandouts - a TRPG tool by WeiHung",
   description: "一個讓你方便分享遊戲手邊資料的簡單工具。",
+  openGraph: {
+    type: "website",
+    locale: "zh_TW",
+    url: defaultUrl,
+    title: "ShareHandouts - a TRPG tool by WeiHung",
+    description: "一個讓你方便分享遊戲手邊資料的簡單工具。",
+    images: [
+      {
+        url: `${defaultUrl}/img/og-img.webp`,
+        width: 1200,
+        height: 630,
+        alt: "ShareHandouts",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ShareHandouts - a TRPG tool by WeiHung",
+    description: "一個讓你方便分享遊戲手邊資料的簡單工具。",
+    images: [
+      {
+        url: `${defaultUrl}/img/og-img.webp`,
+        width: 1200,
+        height: 630,
+        alt: "ShareHandouts",
+      },
+    ],
+  },
 };
 
 const font = Noto_Sans_TC({
