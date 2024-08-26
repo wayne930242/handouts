806b83e1f2cc5db059a957edff3ffe3fca12fad7
Wei Hung <wayne930242@gmail.com>
2024-08-26 23:00:01 +0800

    Fix typpo

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
       <p>
-        Powered by{" "}
+        Developed by{" "}
         <a
           href="https://www.patreon.com/weihung"
           target="_blank"
