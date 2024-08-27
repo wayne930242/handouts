55c8e2e05bcf0afd638154d07d3578fd0382abb6
Wei Hung <wayne930242@gmail.com>
2024-08-27 12:00:49 +0800

    Make image compress

--- app/campaigns/[id]/CampaignEditor/ContentEditor/TextEditor.tsx
+++ app/campaigns/[id]/CampaignEditor/ContentEditor/TextEditor.tsx
@@ -3,12 +3,19 @@
 import { toast } from "@/components/ui/use-toast";
 import { ContentFieldProps } from "../HandoutCard";
 import MyMDXEditor from "@/components/MyMDXEditor";
-import useIsWhitelisted from "@/lib/hooks/useIsWhitelisted";
 import { createClient } from "@/lib/supabase/client";
 import ImageManager from "@/lib/supabase/ImageMamager";
+import imageCompression from "browser-image-compression";
+
+const options = {
+  maxSizeMB: 1,
+  maxWidthOrHeight: 1920,
+  useWebWorker: true,
+  fileType: "image/jpeg",
+  quality: 0.8,
+};
 
 export default function TextEditor({ field, oldValue, campaignId }: Props) {
-  const isWhitelisted = useIsWhitelisted();
   const supabase = createClient();
   const imageManager = new ImageManager(supabase);
 
@@ -18,18 +25,22 @@ export default function TextEditor({ field, oldValue, campaignId }: Props) {
       oldMarkdown={oldValue}
       onChange={(value) => field.onChange(value)}
       imageUploadHandler={
-        campaignId && isWhitelisted
-          ? (image: File) => {
+        campaignId
+          ? async (image: File) => {
               try {
-                const url = imageManager.uploadImage(image, campaignId);
-                return Promise.resolve(url);
+                const compressedImage = await imageCompression(image, options);
+                const url = imageManager.uploadImage(
+                  compressedImage,
+                  campaignId
+                );
+                return url;
               } catch (error) {
                 toast({
                   title: "上傳失敗",
                   description: "圖片上傳失敗，請稍後再試。",
                   variant: "destructive",
                 });
-                return Promise.reject(error);
+                throw error;
               }
             }
           : undefined
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,14 @@ const defaultUrl = "https://handouts.wayneh.tw";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "ShareHandouts - a TRPG tool by WeiHung",
+  title: "Handouts - a TRPG tool by WeiHung",
   description:
     "一個讓你方便分享、管理 TRPG 遊戲的手邊資料的簡單工具。由洪偉開發製作。",
   openGraph: {
     type: "website",
     locale: "zh_TW",
     url: defaultUrl,
-    title: "ShareHandouts - a TRPG tool by WeiHung",
+    title: "Handouts - a TRPG tool by WeiHung",
     description:
       "一個讓你方便分享、管理 TRPG 遊戲的手邊資料的簡單工具。由洪偉開發製作。",
     images: [
@@ -22,13 +22,13 @@ export const metadata: Metadata = {
         url: `${defaultUrl}/img/og-img.webp`,
         width: 1200,
         height: 714,
-        alt: "ShareHandouts",
+        alt: "Handouts",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "ShareHandouts - a TRPG tool by WeiHung",
+    title: "Handouts - a TRPG tool by WeiHung",
     description:
       "一個讓你方便分享、管理 TRPG 遊戲的手邊資料的簡單工具。由洪偉開發製作。",
     images: [
