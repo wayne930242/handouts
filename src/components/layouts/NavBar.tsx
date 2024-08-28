2f065950c38f5804700dc6aea57a195ef3700eac
wayne930242 <wayne930242@gmail.com>
2024-08-28 19:44:38 +0800

    Beta position

--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -4,13 +4,14 @@ import { Badge } from "../ui/badge";
 import LocaleSwitcher from "../LocaleSwitcher";
 
 export default function NavBar({}) {
-
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-12">
       <div className="w-full max-w-4xl flex justify-between items-center py-2 px-3 text-sm">
-        <Link href="/" className="flex items-center gap-1">
+        <Link href="/" className="flex items-center gap-1 relative">
           <div className="text-2xl font-bold">Handouts</div>
-          <Badge variant="destructive">BETA</Badge>
+          <Badge className="absolute top-4 -right-11 px-1.5 py-0.5" variant="destructive">
+            BETA
+          </Badge>
         </Link>
         <div className="flex gap-x-2.5 items-center">
           <LocaleSwitcher />
