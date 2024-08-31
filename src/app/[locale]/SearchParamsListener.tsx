09dc47083c6be951cc35b6e6dc8159793ac6ca99
wayne930242 <wayne930242@gmail.com>
2024-08-31 20:21:46 +0800

    Add router transition

--- src/app/[locale]/SearchParamsListener.tsx
+++ src/app/[locale]/SearchParamsListener.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import OverlayLoading from "@/components/OverlayLoading";
 import { checkPassphraseExists } from "@/lib/passphraseCli";
 import useAppStore from "@/lib/store/useAppStore";
 import { useRouter } from "@/navigation";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useTransition } from "react";
 
 export default function SearchParamsListener() {
   const searchParams = useSearchParams();
@@ -12,26 +13,29 @@ export default function SearchParamsListener() {
     setPassphraseDialog: state.setAddPassphraseDialog,
   }));
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
-    if (searchParams.get("campaign_id")) {
-      const id = searchParams.get("campaign_id") as string;
-      const isExists = checkPassphraseExists(id, "campaigns");
-      if (isExists) {
-        router.push(`/campaigns/${id}`);
-      } else {
-        setPassphraseDialog("campaigns");
+    startTransition(() => {
+      if (searchParams.get("campaign_id")) {
+        const id = searchParams.get("campaign_id") as string;
+        const isExists = checkPassphraseExists(id, "campaigns");
+        if (isExists) {
+          router.push(`/campaigns/${id}`);
+        } else {
+          setPassphraseDialog("campaigns");
+        }
+      } else if (searchParams.get("doc_id")) {
+        const id = searchParams.get("doc_id") as string;
+        const isExists = checkPassphraseExists(id, "docs");
+        if (isExists) {
+          router.push(`/docs/${id}`);
+        } else {
+          setPassphraseDialog("docs");
+        }
       }
-    } else if (searchParams.get("doc_id")) {
-      const id = searchParams.get("doc_id") as string;
-      const isExists = checkPassphraseExists(id, "docs");
-      if (isExists) {
-        router.push(`/docs/${id}`);
-      } else {
-        setPassphraseDialog("docs");
-      }
-    }
+    });
   }, [searchParams]);
 
-  return <></>;
+  return isPending ? <OverlayLoading /> : null;
 }
