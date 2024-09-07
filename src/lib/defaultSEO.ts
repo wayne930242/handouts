d9cf69403af9b53ff44ba91e5ef13453d63a5d91
Wei Hung <wayne930242@gmail.com>
2024-09-07 19:02:56 +0800

    Fix order

--- src/lib/defaultSEO.ts
+++ src/lib/defaultSEO.ts
@@ -40,13 +40,13 @@ export const genSEO = async ({
       url: url ?? defaultUrl,
       title: title ?? t("title"),
       description: description ?? t("description"),
-      images: [...(images ?? [defaultImage])],
+      images: [defaultImage, ...(images ?? [])],
     },
     twitter: {
       card: "summary_large_image",
       title: title ?? t("title"),
       description: description ?? t("description"),
-      images: [...(images ?? [defaultImage])],
+      images: [defaultImage, ...(images ?? [])],
     },
   };
 };
