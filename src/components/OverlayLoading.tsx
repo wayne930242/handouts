724d3349eb9dd5407bafa291b2e777ba6d074e5e
Wei Hung <wayne930242@gmail.com>
2024-09-07 17:29:27 +0800

    Mod overlay loading style

--- src/components/OverlayLoading.tsx
+++ src/components/OverlayLoading.tsx
@@ -4,7 +4,7 @@ import { PacmanLoader } from "react-spinners";
 
 export default function OverlayLoading() {
   return (
-    <div className="fixed inset-0 bg-foreground/30 flex justify-center items-center z-[9999] pointer-events-none">
+    <div className="fixed inset-0 bg-foreground/60 flex justify-center items-center z-[9999] pointer-events-none">
       <PacmanLoader color="hsl(var(--foreground))" loading={true} />
     </div>
   );
