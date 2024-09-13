1c39b05833f9fe3d6b7c7a7efd011ca5c8a25750
wayne930242 <wayne930242@gmail.com>
2024-09-13 22:36:35 +0800

    Fix sticky toc

--- src/components/toc/TocContainer.tsx
+++ src/components/toc/TocContainer.tsx
@@ -7,6 +7,7 @@ import {
 } from "../ui/dropdown-menu";
 import { Button } from "../ui/button";
 import { ArrowUpToLine, TableOfContents } from "lucide-react";
+import Sticky from "react-sticky-el";
 import useSmoothScroll from "@/lib/hooks/useSmoothScroll";
 
 interface Props {
@@ -24,22 +25,26 @@ export default function TocContainer({
 
   return (
     <>
-      <div className="hidden md:flex w-[305px] sticky top-0 overflow-y-auto mr-4 px-4 py-2 flex-col items-center h-layout border-border rounded-lg border">
-        <div className="w-full">
-          <a
-            className="toc-link text-center flex items-center justify-center mb-2"
-            href={`#${topId}`}
-          >
-            <Button
-              size="sm"
-              variant="link"
-              className="w-full flex items-center justify-center text-foreground hover:text-[#007bff]"
-            >
-              <ArrowUpToLine className="w-5 h-5" />
-            </Button>
-          </a>
-        </div>
-        {children}
+      <div className="w-[305px] mr-4">
+        <Sticky>
+          <div className="w-full hidden px-4 py-2 md:flex top-0 overflow-y-auto flex-col items-center h-layout border-border rounded-lg border">
+            <div className="w-full">
+              <a
+                className="toc-link text-center flex items-center justify-center mb-2"
+                href={`#${topId}`}
+              >
+                <Button
+                  size="sm"
+                  variant="link"
+                  className="w-full flex items-center justify-center text-foreground hover:text-[#007bff]"
+                >
+                  <ArrowUpToLine className="w-5 h-5" />
+                </Button>
+              </a>
+            </div>
+            {children}
+          </div>
+        </Sticky>
       </div>
       <div className="fixed bottom-5 left-5 z-50 md:hidden block">
         <DropdownMenu modal={false}>
